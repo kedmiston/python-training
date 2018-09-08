c9970fcb62ba57216527ac77001039a70e5f6a4d
kedmiston <kedmist@gmail.com>
2018-09-08 09:12:43 -0500

    Worked on and finished circle exercise

---
+++ python_morsels_week_7_circle/circle.py
@@ -0,0 +1,31 @@
+import math
+
+
+class Circle():
+    def __init__(self, radius=1):
+        self.radius = radius
+
+    def __repr__(self):
+        return 'Circle({})'.format(self.radius)
+
+    @property
+    def area(self):
+        return math.pi * self.radius ** 2
+
+    @property
+    def diameter(self):
+        return self.radius * 2
+
+    @diameter.setter
+    def diameter(self, diameter):
+        self.radius = diameter / 2
+
+    @property
+    def radius(self):
+        return self._radius
+
+    @radius.setter
+    def radius(self, radius):
+        if radius < 0:
+            raise ValueError('Radius cannot be negative')
+        self._radius = radius
